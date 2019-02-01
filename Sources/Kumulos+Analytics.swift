c5eb7995693312291225e04202e18054f8b0aabf
cgwyllie <cgwyllie@gmail.com>
2019-02-01 14:11:29 +0000

    Persist userIdentifier to NSUserDefaults on association and add accessor (#27)
    * Refactor user association implementation to shared helper
    
    * Write user identifier to UserDefaults during association
    
    * Add accessor for currently associated user identifier

--- Sources/Kumulos+Analytics.swift
+++ Sources/Kumulos+Analytics.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 public extension Kumulos {
+
+    private static let userIdLock = DispatchSemaphore(value: 1)
+    internal static let USER_ID_KEY = "KumulosCurrentUserID"
     
     internal static func trackEvent(eventType: KumulosEvent, properties: [String:Any]?, immediateFlush: Bool = false) {
         getInstance().analyticsHelper?.trackEvent(eventType: eventType.rawValue, properties: properties, immediateFlush: immediateFlush)
@@ -42,13 +45,7 @@ public extension Kumulos {
      - userIdentifier: Unique identifier for the current user
      */
     public static func associateUserWithInstall(userIdentifier: String) {
-        if userIdentifier == "" {
-            print("User identifier cannot be empty, aborting!")
-            return
-        }
-
-        let params = ["id": userIdentifier]
-        Kumulos.trackEvent(eventType: KumulosEvent.STATS_ASSOCIATE_USER, properties: params as [String : AnyObject], immediateFlush: true)
+        associateUserWithInstallImpl(userIdentifier: userIdentifier, attributes: nil)
     }
     
     /**
@@ -59,12 +56,44 @@ public extension Kumulos {
      - attributes: JSON encodable dictionary of attributes to store for the user
      */
     public static func associateUserWithInstall(userIdentifier: String, attributes: [String:AnyObject]) {
+        associateUserWithInstallImpl(userIdentifier: userIdentifier, attributes: attributes)
+    }
+
+    /**
+     Returns the identifier for the user currently associated with the Kumulos installation record
+
+     If no user is associated, it returns the Kumulos installation ID
+    */
+    public static var currentUserIdentifier : String {
+        get {
+            userIdLock.wait()
+            defer { userIdLock.signal() }
+            if let userId = UserDefaults.standard.value(forKey: USER_ID_KEY) as! String? {
+                return userId;
+            }
+
+            return Kumulos.installId
+        }
+    }
+
+    fileprivate static func associateUserWithInstallImpl(userIdentifier: String, attributes: [String:AnyObject]?) {
         if userIdentifier == "" {
             print("User identifier cannot be empty, aborting!")
             return
         }
-        
-        let params = ["id": userIdentifier, "attributes": attributes] as [String : Any]
+
+        var params : [String:Any]
+        if let attrs = attributes {
+            params = ["id": userIdentifier, "attributes": attrs]
+        }
+        else {
+            params = ["id": userIdentifier]
+        }
+
+        userIdLock.wait()
+        UserDefaults.standard.set(userIdentifier, forKey: USER_ID_KEY)
+        userIdLock.signal()
+
         Kumulos.trackEvent(eventType: KumulosEvent.STATS_ASSOCIATE_USER, properties: params, immediateFlush: true)
     }
     
