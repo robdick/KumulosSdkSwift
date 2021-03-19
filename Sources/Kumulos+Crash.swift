10ab203299fcfe974d7ee85e83de4e46c0abc60d
Rob Dick <r.dick@kumulos.com>
2021-03-19 10:18:08 +0000

    Restore crash ref

--- Sources/Kumulos+Crash.swift
+++ Sources/Kumulos+Crash.swift
@@ -7,39 +7,39 @@
 //
 
 import Foundation
-//import KSCrash
+import KSCrash_Installations
 
 public extension Kumulos {
-    
+
     /**
      Send any pending reports to the API
      */
     internal func trackAndReportCrashes() {
-//        let url =  "\(baseCrashUrl)/track/\(apiKey)/kscrash/\(Kumulos.installId)"
-//
-//        let installation = KSCrashInstallationStandard.sharedInstance()
-//        installation?.url = URL(string: url)
-//
-//        installation?.install()
-//
-//        installation?.sendAllReports { (reports, completed, error) -> Void in
-//            if(completed) {
-//                print("Sent \(String(describing: reports?.count)) reports")
-//            } else {
-//                print("Failed to send reports: \(String(describing: error))")
-//            }
-//        }
+       let url =  "\(baseCrashUrl)/track/\(apiKey)/kscrash/\(Kumulos.installId)"
+
+       let installation = KSCrashInstallationStandard.sharedInstance()
+       installation?.url = URL(string: url)
+
+       installation?.install()
+
+       installation?.sendAllReports { (reports, completed, error) -> Void in
+           if(completed) {
+               print("Sent \(String(describing: reports?.count)) reports")
+           } else {
+               print("Failed to send reports: \(String(describing: error))")
+           }
+       }
     }
-    
+
     static func logException(name: String, reason: String, language: String, lineOfCode: String, stackTrace: [Any], logAllThreads: Bool)  {
-//        KSCrash.sharedInstance().reportUserException(name, reason: reason, language: language, lineOfCode: lineOfCode, stackTrace: stackTrace, logAllThreads: logAllThreads, terminateProgram: false)
-//
-//        KSCrash.sharedInstance().sendAllReports{ (reports, completed, error) -> Void in
-//            if(completed) {
-//                print("Sent \(String(describing: reports?.count)) reports")
-//            } else {
-//                print("Failed to send reports: \(String(describing: error))")
-//            }
-//        }
+       KSCrash.sharedInstance().reportUserException(name, reason: reason, language: language, lineOfCode: lineOfCode, stackTrace: stackTrace, logAllThreads: logAllThreads, terminateProgram: false)
+
+       KSCrash.sharedInstance().sendAllReports{ (reports, completed, error) -> Void in
+           if(completed) {
+               print("Sent \(String(describing: reports?.count)) reports")
+           } else {
+               print("Failed to send reports: \(String(describing: error))")
+           }
+       }
     }
 }
